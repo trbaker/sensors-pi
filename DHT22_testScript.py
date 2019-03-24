d3c154a77e8346bb06a26397a7ed1b51172e6b1d
Tom Baker <tbaker@ku.edu>
2019-03-24 10:09:31 -0500

    Add files via upload

---
+++ DHT22_testScript.py
@@ -0,0 +1,14 @@
+import Adafruit_DHT
+sensor = Adafruit_DHT.DHT22
+
+#This is the GPIO Pin number, not just the Pin number.
+#This pin in pin 7 but sits in GPIO Pin 4. Use 4 below.
+pin = 4
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+temperature = temperature * 9/5.0 + 32
+
+if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+print('Failed to get reading. Try again!')
