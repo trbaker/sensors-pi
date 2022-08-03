7afcb26ac17dd4e96e7574ac3917cb7661f596a6
Tom R Baker <trbaker@gmail.com>
2022-08-03 08:28:57 -0500

    Update send_to_AGO.py

--- send_to_AGO.py
+++ send_to_AGO.py
@@ -4,8 +4,11 @@ import json
 url = 'https://services3.arcgis.com/GzteEaZqBuJ6GIYr/arcgis/rest/services/survey123_910b6ea1c50743269a5b171a91fe6cc7_fieldworker/FeatureServer/0/addFeatures'
 params={"f":"pjson","token":"","rollbackOnFailure":"false","features":'{ \
         "attributes" : { \
-            "controller" : "My controller 2", \
-            "Longitude": -94.2342}}'
+            "controller" : "My test controller 2", \
+            "lat" : -94.23434, \
+            "lon": -94.2342, \
+            "temperature" : 0, \
+            "humidity" : 0}}' 
             }
 
 x = requests.post(url, params=params)
