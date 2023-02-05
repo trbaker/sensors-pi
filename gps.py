b1a852408fe30b5596b59e9e4c5f3e2c052175d1
Tom R Baker <trbaker@gmail.com>
2023-02-05 07:40:44 -0600

    Add files via upload

---
+++ gps.py
@@ -0,0 +1,17 @@
+from gps3 import gps3
+import time
+
+
+gps_socket = gps3.GPSDSocket()
+data_stream = gps3.DataStream()
+gps_socket.connect()
+gps_socket.watch()
+for new_data in gps_socket:
+    if new_data:
+        data_stream.unpack(new_data)
+        print(' ')
+        print('Altitude = ', data_stream.TPV['alt'])
+        print('Latitude = ', data_stream.TPV['lat'])
+        print('Longitude = ', data_stream.TPV['lon'])
+        print(' ')
+        
